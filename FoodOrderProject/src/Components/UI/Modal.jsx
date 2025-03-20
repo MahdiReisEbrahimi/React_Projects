b393f4a395749196bfc4628ff9ce8deb715f3acb
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-03-20 15:41:33 +0330

    repairing onSpace Click logic on cart modal

--- FoodOrderProject/src/Components/UI/Modal.jsx
+++ FoodOrderProject/src/Components/UI/Modal.jsx
@@ -1,20 +1,41 @@
-import { Fragment } from 'react';
-import ReactDOM from 'react-dom';
+import { Fragment, useRef, useEffect, useContext } from "react";
+import ReactDOM from "react-dom";
+import AuthContext from "../../Context/Auth-context";
+import classes from "./Modal.module.css";
 
-import classes from './Modal.module.css';
+const Backdrop = () => {
+  const ctx = useContext(AuthContext);
 
-const Backdrop = (props) => {
-  return <div className={classes.backdrop} />;
+  return <div className={classes.backdrop} onClick={ctx.onSpaceClick} />;
 };
 
 const ModalOverlay = (props) => {
+  const modalRef = useRef(null);
+  const ctx = useContext(AuthContext);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (modalRef.current && !modalRef.current.contains(event.target)) {
+        ctx.onSpaceClick();
+      }
+    };
+
+    // با هر کلیک موس یکبار تابع بالا رو اجرا کن
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [ctx]);
+
   return (
-    <div className={classes.modal}>
+    <div ref={modalRef} className={classes.modal}>
       <div className={classes.content}>{props.children}</div>
     </div>
   );
 };
-const portalElement = document.getElementById('overlays');
+
+const portalElement = document.getElementById("overlays");
 
 const Modal = (props) => {
   return (
