e6981ea1eb0f4dc511259ecd7ce57d8c783503cc
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-04-06 19:47:23 +0330

    adding Remove and show counter logics.

--- Redux/src/Components/ReduxCounter.jsx
+++ Redux/src/Components/ReduxCounter.jsx
@@ -2,6 +2,8 @@ import { useSelector, useDispatch } from "react-redux";
 import { useRef } from "react";
 export default function ReduxCounter() {
   const counter = useSelector((state) => state.counter);
+
+  const showCounter = useSelector((state) => state.showCounter);
   const dispatch = useDispatch();
 
   const inputRef = useRef();
@@ -19,40 +21,54 @@ export default function ReduxCounter() {
     inputRef.current.value = "";
   }
 
+  function deleteShowCounterHandler() {
+    dispatch({ type: "deleteShowCounter" });
+  }
+
   return (
     <div className="rounded-2xl font-bold w-1/2 bg-white shadow-lg shadow-gray-400 m-auto flex flex-col items-center p-10 text-purple-700">
-      <p className="p-3 text-2xl mb-7">REDUX COUNTER</p>
-      <h2>{counter}</h2>
-      <div className="flex flex-col justify-center w-full">
-        <button
-          onClick={incrementHandler}
-          className=" m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
-        >
-          increment (+1)
-        </button>
-        <div className="flex flex-row items-center">
-          <button
-            onClick={increaseHandler}
-            className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
-          >
-            increase by
-          </button>
-          <input
-            className=" text-black bg-blue-300 rounded-2xl m-2 h-15 p-2"
-            type="number"
-            ref={inputRef}
-          />
+      {showCounter && (
+        <div>
+          <p className="p-3 text-2xl mb-7">REDUX COUNTER</p>
+          <h2>{counter}</h2>
+          <div className="flex flex-col justify-center w-full">
+            <button
+              onClick={incrementHandler}
+              className=" m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
+            >
+              increment (+1)
+            </button>
+            <div className="flex flex-row items-center">
+              <button
+                onClick={increaseHandler}
+                className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
+              >
+                increase by
+              </button>
+              <input
+                className=" text-black bg-blue-300 rounded-2xl m-2 h-15 p-2"
+                type="number"
+                ref={inputRef}
+              />
+            </div>
+            <button
+              onClick={decrementHandler}
+              className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
+            >
+              decrement (-1)
+            </button>
+          </div>
         </div>
-        <button
-          onClick={decrementHandler}
-          className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
-        >
-          decrement (-1)
-        </button>
-      </div>
-
-      <button className="cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold">
-        Toggle Counter
+      )}
+      <button
+        onClick={deleteShowCounterHandler}
+        className={`cursor-pointer ${
+          showCounter
+            ? "hover:bg-red-700 bg-red-500"
+            : "hover:bg-green-800 bg-green-600"
+        } bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold`}
+      >
+        {showCounter ? "REMOVE COUNTER" : "SHOW COUNTER"}
       </button>
     </div>
   );
--- Redux/src/Store/index.js
+++ Redux/src/Store/index.js
@@ -1,10 +1,19 @@
 import { createStore } from "redux";
 
-function counterReducer(state = { counter: 0 }, action) {
-  if (action.type === "increment") return { counter: state.counter + 1 };
-  if (action.type === "dec") return { counter: state.counter - 1 };
+const initialData = { counter: 0, showCounter: true };
+
+function counterReducer(state = initialData, action) {
+  if (action.type === "increment")
+    return { counter: state.counter + 1, showCounter: state.showCounter };
+  if (action.type === "dec")
+    return { counter: state.counter - 1, showCounter: state.showCounter };
   if (action.type === "increase")
-    return { counter: state.counter + action.amount };
+    return {
+      counter: state.counter + action.amount,
+      showCounter: state.showCounter,
+    };
+  if (action.type === "deleteShowCounter")
+    return { showCounter: !state.showCounter, counter: state.counter };
   return state;
 }
 
