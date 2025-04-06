179c01cc10515e5959183d8c488f2f6f70bbf5b3
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-04-06 19:26:50 +0330

    adding action.amount to reducer Function and adding a new input and button for increase by...

--- Redux/src/Components/ReduxCounter.jsx
+++ Redux/src/Components/ReduxCounter.jsx
@@ -1,33 +1,53 @@
 import { useSelector, useDispatch } from "react-redux";
-
+import { useRef } from "react";
 export default function ReduxCounter() {
   const counter = useSelector((state) => state.counter);
   const dispatch = useDispatch();
 
+  const inputRef = useRef();
+
   function incrementHandler() {
-    dispatch({ type: "inc" });
+    dispatch({ type: "increment" });
   }
 
   function decrementHandler() {
     dispatch({ type: "dec" });
   }
 
+  function increaseHandler() {
+    dispatch({ type: "increase", amount: Number(inputRef.current.value) });
+    inputRef.current.value = "";
+  }
+
   return (
     <div className="rounded-2xl font-bold w-1/2 bg-white shadow-lg shadow-gray-400 m-auto flex flex-col items-center p-10 text-purple-700">
       <p className="p-3 text-2xl mb-7">REDUX COUNTER</p>
       <h2>{counter}</h2>
-      <div className="flex flex-row justify-center w-full">
+      <div className="flex flex-col justify-center w-full">
         <button
           onClick={incrementHandler}
           className=" m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
         >
-          increment
+          increment (+1)
         </button>
+        <div className="flex flex-row items-center">
+          <button
+            onClick={increaseHandler}
+            className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
+          >
+            increase by
+          </button>
+          <input
+            className=" text-black bg-blue-300 rounded-2xl m-2 h-15 p-2"
+            type="number"
+            ref={inputRef}
+          />
+        </div>
         <button
           onClick={decrementHandler}
           className="m-2 cursor-pointer hover:bg-purple-700 bg-gray-800 p-3 rounded-2xl mt-5 text-white font-bold"
         >
-          decrement
+          decrement (-1)
         </button>
       </div>
 
--- Redux/src/Store/index.js
+++ Redux/src/Store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "redux";
 
 function counterReducer(state = { counter: 0 }, action) {
-  if (action.type === "inc") return { counter: state.counter + 1 };
+  if (action.type === "increment") return { counter: state.counter + 1 };
   if (action.type === "dec") return { counter: state.counter - 1 };
+  if (action.type === "increase")
+    return { counter: state.counter + action.amount };
   return state;
 }
 
