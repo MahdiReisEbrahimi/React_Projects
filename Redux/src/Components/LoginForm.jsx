229888792e44c981781373729924c2b4144c4bc2
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-04-08 00:02:49 +0330

    Form Action added to loginForm component and handled form submition. redux is not involved yet...

--- Redux/src/Components/LoginForm.jsx
+++ Redux/src/Components/LoginForm.jsx
@@ -1,8 +1,34 @@
-export default function LoginForm() {
+import { useActionState } from "react";
 
+function loginAction(prevLoginState, formData) {
+  const enteredUser = formData.get("user");
+  const enteredPass = formData.get("pass");
+
+  const errors = [];
+
+  if (!enteredUser.trim()) errors.push("You missed UserName!");
+  if (!enteredPass.trim()) errors.push("You missed Password!");
+
+  if (errors.length > 0)
+    return {
+      errors,
+      enteredValues: { enteredUser: enteredUser, enteredPass: enteredPass },
+    };
+
+  console.log(enteredUser + " : " + enteredPass);
+  return {
+    errors: null,
+  };
+}
+
+export default function LoginForm() {
+  const [formState, formAction] = useActionState(loginAction, { errors: null });
 
   return (
-    <form className="rounded-lg w-1/2 m-auto border-2 border-white p-5 mt-25 flex flex-col items-center">
+    <form
+      action={formAction}
+      className="rounded-lg w-1/2 m-auto border-2 border-white p-5 mt-25 flex flex-col items-center"
+    >
       <div className="flex flex-col w-full">
         <label htmlFor="user" className="text-white ">
           UserName
@@ -12,6 +38,7 @@ export default function LoginForm() {
           name="user"
           id="user"
           className="w-full mb-3 bg-white rounded-lg p-2 pt-1 pb-1"
+          defaultValue={formState?.enteredValues?.enteredUser}
         />
       </div>
 
@@ -24,9 +51,16 @@ export default function LoginForm() {
           name="pass"
           id="pass"
           className="w-full bg-white rounded-lg p-2 pt-1 pb-1"
+          defaultValue={formState?.enteredValues?.enteredPass}
         />
       </div>
-
+      {formState?.errors && (
+        <ul className="text-red-500 bg-white p-2 rounded-lg mt-3">
+          {formState.errors.map((error) => {
+            return <li key={error}>{error}</li>;
+          })}
+        </ul>
+      )}
       <button className="cursor-pointer font-bold hover:bg-blue-300 w-1/2  bg-white rounded-lg p-2 mt-4">
         SUBMIT
       </button>
