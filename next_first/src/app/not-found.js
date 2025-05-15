3aa5393f511b3157767fc9b12fbfb92df75aecd5
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-05-15 14:52:25 +0330

    Design for not-found urls aded.

---
+++ next_first/src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function notFound() {
+  return (
+    <div className="flex justify-center mt-30">
+      <h1 className="text-orange-500 font-bold text-3xl ">
+        We couldnt find the page which is requested...!
+      </h1>
+    </div>
+  );
+}
