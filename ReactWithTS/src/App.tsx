f1e0e052704aaa34ea87168e830618242a3169c9
Mahdi Ebrahimi <Mhdiebrahimim@gmail.com>
2025-05-26 22:08:51 +0330

    two typescript+react components aded.

--- ReactWithTS/src/App.tsx
+++ ReactWithTS/src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
+
+const items = [new Todo("Learn React"), new Todo("Learn TypeScript")];
 
 function App() {
   return (
-    <div>
-      <Todos />
+    <div className="m-10 p-30">
+      <Todos items={items} />
     </div>
   );
 }
--- ReactWithTS/src/components/Todos.tsx
+++ ReactWithTS/src/components/Todos.tsx
@@ -1,10 +1,15 @@
 import React from "react";
+import Todo from "../models/todo";
+import TodoPrint from "./TodosPrint";
 
-export default function Todos() {
+const Todos: React.FC<{ items: Todo[] }> = ({ items }) => {
   return (
     <ul>
-      <li>Learning</li>
-      <li>Doing</li>
+      {items.map((item) => (
+        <TodoPrint key={item.id} item={item} />
+      ))}
     </ul>
   );
-}
+};
+
+export default Todos;
